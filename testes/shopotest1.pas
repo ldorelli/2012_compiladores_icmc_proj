35a901b6cf0b13087d81331124fc48056da78a2d
Luis Fernando Dorelli de Abreu <lfdorelli@Luiss-MacBook-Air.local>
2012-06-03 22:39:15 -0300

    Novos testes

---
+++ testes/shopotest1.pas
@@ -0,0 +1,23 @@
+program procedure t;
+{ procedure g (); }
+const T = 333;
+const T_X := 345
+const var Y;
+
+procedura procedimental (procedurando var t)
+begin
+end;
+
+var xyz : integer;
+var leite ninho : integral;
+
+procedure ploplo(correta : char) ;
+begin
+	printf("CARAMBA");
+	writeln(work)
+	readln(454 + 222)
+end;\.
+
+begin
+
+end;
