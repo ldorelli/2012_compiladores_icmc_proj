f9ab8f221977f44fb4c056bd1d6a511df5846498
Luis Fernando Dorelli de Abreu <lfdorelli@Luiss-MacBook-Air.local>
2012-06-04 17:53:25 -0300

    Diretorio added

---
+++ Entrega2/y.tab.h
@@ -0,0 +1,145 @@
+/* A Bison parser, made by GNU Bison 2.3.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     IDENT = 258,
+     OP_AT = 259,
+     OP_DF = 260,
+     OP_GE = 261,
+     OP_LE = 262,
+     OP_GR = 263,
+     OP_LS = 264,
+     OP_PL = 265,
+     OP_MI = 266,
+     OP_ML = 267,
+     OP_DV = 268,
+     OP_EQ = 269,
+     SB_PV = 270,
+     SB_PF = 271,
+     SB_DP = 272,
+     SB_VG = 273,
+     SB_PO = 274,
+     SB_PC = 275,
+     ER_IDG = 276,
+     ER_IFL = 277,
+     ER_NMF = 278,
+     ER_CIN = 279,
+     ER_CNF = 280,
+     NRO_INTEIRO = 281,
+     NRO_REAL = 282,
+     PROGRAM = 283,
+     BEG = 284,
+     END = 285,
+     CONST = 286,
+     VAR = 287,
+     REAL = 288,
+     INTEGER = 289,
+     CHAR = 290,
+     PROCEDURE = 291,
+     IF = 292,
+     ELSE = 293,
+     READLN = 294,
+     WRITELN = 295,
+     REPEAT = 296,
+     THEN = 297,
+     UNTIL = 298,
+     WHILE = 299,
+     DO = 300,
+     FUNCTION = 301
+   };
+#endif
+/* Tokens.  */
+#define IDENT 258
+#define OP_AT 259
+#define OP_DF 260
+#define OP_GE 261
+#define OP_LE 262
+#define OP_GR 263
+#define OP_LS 264
+#define OP_PL 265
+#define OP_MI 266
+#define OP_ML 267
+#define OP_DV 268
+#define OP_EQ 269
+#define SB_PV 270
+#define SB_PF 271
+#define SB_DP 272
+#define SB_VG 273
+#define SB_PO 274
+#define SB_PC 275
+#define ER_IDG 276
+#define ER_IFL 277
+#define ER_NMF 278
+#define ER_CIN 279
+#define ER_CNF 280
+#define NRO_INTEIRO 281
+#define NRO_REAL 282
+#define PROGRAM 283
+#define BEG 284
+#define END 285
+#define CONST 286
+#define VAR 287
+#define REAL 288
+#define INTEGER 289
+#define CHAR 290
+#define PROCEDURE 291
+#define IF 292
+#define ELSE 293
+#define READLN 294
+#define WRITELN 295
+#define REPEAT 296
+#define THEN 297
+#define UNTIL 298
+#define WHILE 299
+#define DO 300
+#define FUNCTION 301
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYSTYPE yylval;
+
